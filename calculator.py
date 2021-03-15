a5a59dda761bcb67e4d5217dcd2e93abef1e7d19
namankumar818 <80568028+namankumar818@users.noreply.github.com>
2021-03-15 17:43:33 +0530

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,25 @@
+# calc
+a = eval(input("enter the number"))
+b = eval(input('enter the number'))
+c = (input('+,-,/,*\n'))
+if c == '+' :
+    print(a+b)
+elif c == '-':
+    print(a-b)
+elif c == '/':
+    print(a/b)
+else :
+    print(a*b)
+    
+
+
+
+
+
+
+     
+
+    
+
+               
+               
