186794850db28aaac36b4c7c0958e0180747bd75
namankumar818 <80568028+namankumar818@users.noreply.github.com>
2021-03-15 15:24:13 +0530

    char_id.py

---
+++ char_id.py
@@ -0,0 +1,11 @@
+a  = input("enter the value:- ")
+if  'A'<=a<'Z' :
+         print("upper case")
+elif 'a'<=a<'z' :
+         print("lower case")
+elif '0'<a<'9' :
+         print("digits")
+else :
+         print("special character")
+
+         
