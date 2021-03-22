206b0279fc53e398d0dd4f79549932ac17bf988a
namankumar818 <80568028+namankumar818@users.noreply.github.com>
2021-03-22 03:06:32 -0700

    armstrong number

---
+++ armstrong_number.py
@@ -0,0 +1,13 @@
+a = input("enter the number")
+ln = len(a)
+s = 0
+i = 0
+while i<ln:
+    s += int(a[i])**ln
+    i += 1
+if int(a) == s:
+    print('number is armstrong')
+else:
+    print('not armstrong')
+    
+ 
